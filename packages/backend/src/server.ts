44e144c889156bf0dde193c1733a284ef62a0a24
Justin Crisp <justin@crispcode.io>
2025-01-03 11:56:13 -0600

    update docker configs

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { router as userRoutes } from './routes/users';
-import authRoutes from './routes/authRoutes';
+import { router as userRoutes } from './routes/users.js';
+import authRoutes from './routes/authRoutes.js';
 import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
